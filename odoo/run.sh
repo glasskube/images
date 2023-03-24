403b0bf1cfbc14dae9362b6ea217eb681e9ae60b
Jakob Steiner <jakob.steiner@glasskube.eu>
2023-03-24 15:13:10 +0100

    fix(Odoo): add asset invalidation on version change

--- odoo/run.sh
+++ odoo/run.sh
@@ -5,6 +5,9 @@ RESULT_NOK=1
 RESULT_FAIL=2
 
 DB_NAME="odoo"
+VOLUME_DIR="/var/lib/odoo"
+VERSION_FILE="$VOLUME_DIR/last_version.txt"
+CURRENT_VERSION=$(odoo --version)
 
 function abort {
   reason=$1
@@ -37,11 +40,33 @@ function update_db {
   /entrypoint.sh "$@" "--update" "all" "-d" "$DB_NAME" "--stop-after-init"
 }
 
+function invalidate_assets {
+  if [ -f "$VERSION_FILE" ]; then
+    LAST_VERSION="$(<"$VERSION_FILE")"
+    echo "Last version was $LAST_VERSION"
+  else
+    LAST_VERSION="n.a."
+    echo "Version file does not exist"
+  fi
+
+  if [ "$CURRENT_VERSION" != "$LAST_VERSION" ]; then
+    echo "Current version differs from previous version ($CURRENT_VERSION =/= $LAST_VERSION)"
+    PGHOST="$HOST" PGUSER="$USER" PGPASSWORD="$PASSWORD" PGDATABASE="$DB_NAME" \
+      psql -qc "delete from ir_attachment where res_model='ir.ui.view' and name like '%assets_%'" \
+      >/dev/null
+  fi
+}
+
+function print_version_file() {
+  printf "%s" "$CURRENT_VERSION" >"$VERSION_FILE"
+}
+
 check_db
 
 case $? in
 "$RESULT_OK")
   echo "Database already initialized"
+  invalidate_assets
   update_db "$@"
   ;;
 "$RESULT_NOK")
@@ -52,5 +77,7 @@ case $? in
   abort "Error connecting to database server"
   ;;
 esac
+
+print_version_file
 
 exec "/entrypoint.sh" "$@"
